447a923bce2ceb152c958a6baf9bc54551d6cdb5
kak!us56 <rudniyyarik@gmail.com>
2025-04-27 13:49:24 +0200

    again fix main.go

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"log"
+    "log"
+    "os"
 
-	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	db "github.com/troodinc/trood-front-hackathon/database"
-	_ "github.com/troodinc/trood-front-hackathon/docs"
-	"github.com/troodinc/trood-front-hackathon/handlers"
+    "github.com/gin-gonic/gin"
+    swaggerFiles "github.com/swaggo/files"
+    ginSwagger "github.com/swaggo/gin-swagger"
+    db "github.com/troodinc/trood-front-hackathon/database"
+    _ "github.com/troodinc/trood-front-hackathon/docs"
+    "github.com/troodinc/trood-front-hackathon/handlers"
 )
 
 // @title Trood Front Hackathon API
@@ -18,28 +19,32 @@ import (
 // @BasePath /
 
 func main() {
-	db.InitDatabase()
-	handlers.InitProjects()
+    db.InitDatabase()
+    handlers.InitProjects()
 
-	r := gin.Default()
+    r := gin.Default()
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.GET("/projects", handlers.GetProjects)
-	r.GET("/projects/:id", handlers.GetProjectByID)
-	r.POST("/projects", handlers.CreateProject)
-	r.PUT("/projects/:id", handlers.EditProject)
-	r.DELETE("/projects/:id", handlers.DeleteProject)
+    r.GET("/projects", handlers.GetProjects)
+    r.GET("/projects/:id", handlers.GetProjectByID)
+    r.POST("/projects", handlers.CreateProject)
+    r.PUT("/projects/:id", handlers.EditProject)
+    r.DELETE("/projects/:id", handlers.DeleteProject)
 
-	r.GET("/projects/:id/vacancies", handlers.GetVacancies)
-	r.POST("/projects/:id/vacancies", handlers.CreateVacancy)
-	r.PUT("/vacancies/:id", handlers.EditVacancy)
-	r.DELETE("/vacancies/:id", handlers.DeleteVacancy)
+    r.GET("/projects/:id/vacancies", handlers.GetVacancies)
+    r.POST("/projects/:id/vacancies", handlers.CreateVacancy)
+    r.PUT("/vacancies/:id", handlers.EditVacancy)
+    r.DELETE("/vacancies/:id", handlers.DeleteVacancy)
 
-	port := "8080"
-	log.Println("Server running on http://localhost:" + port)
+    // >>> здесь берем PORT из переменных окружения
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // если PORT не задан, используем 8080
+    }
+    log.Println("Server running on http://localhost:" + port)
 
-	if err := r.Run(":" + port); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+    if err := r.Run(":" + port); err != nil {
+        log.Fatalf("Server failed to start: %v", err)
+    }
 }
